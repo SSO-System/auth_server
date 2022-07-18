dd0ff1f34172ce6ec1e608fda2d20acd613503bc
dang luan <dangluan15112001@gmail.com>
2022-07-18 16:46:49 +0700

    user register

--- src/routes/auth.router.ts
+++ src/routes/auth.router.ts
@@ -4,16 +4,17 @@ import { Provider } from "oidc-provider";
 import authController from "../controllers/auth.controller";
 // import { onlyClient } from "../middlewares/auth.middleware";
 import { noCache } from "../middlewares/no-cache.middleware";
-
 const bodyParser = koaBody();
 
 export default (oidc: Provider) => {
   const router = new Router();
 
-  const { abortInteraction, confirmInteraction, interaction, login, register, registerForm, checkSession } =
+  const { abortInteraction, confirmInteraction, interaction, login, register, registerForm, checkRegister, checkSession } =
     authController(oidc);
-  router.get('/register', registerForm);
-  router.post("/users", bodyParser, register);
+  
+  router.get('/interaction/:uid/register1', registerForm);
+  router.post('/checkRegister',bodyParser, checkRegister);
+  router.post("/interaction/:uid/register", bodyParser, register);
 
   router.post("/interaction/:uid/login", noCache, bodyParser, login);
   router.post("/interaction/:uid/confirm", noCache, confirmInteraction);
