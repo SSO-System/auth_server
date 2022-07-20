74fcb5843458dc1c6ce15752e4e31d1b8f50b6c9
Gia Cat Nguyen Khoa <giacatnk@gmail.com>
2022-07-20 17:10:47 +0700

    .env

--- src/db/postgresql/connection.ts
+++ src/db/postgresql/connection.ts
@@ -1,10 +1,10 @@
 import { Pool } from 'pg';
 
 let pool = new Pool({
-        user: 'postgres',
-        host: 'localhost',
-        database: 'DEMO SSO',
-        password: 'giacat',
+        user: 'sso',
+        host: '42.117.5.115',
+        database: 'INTERN-SSO',
+        password: 'dientoan@123',
         port: '5432'
 });
 
