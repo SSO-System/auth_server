16cf4eb29eb4723c10c19b4a7403ac2f7d3142f3
Gia Cat Nguyen Khoa <giacatnk@gmail.com>
2022-07-21 17:51:11 +0700

    Fix some bugs

--- src/controllers/auth.controller/login.ts
+++ src/controllers/auth.controller/login.ts
@@ -1,34 +1,39 @@
 import * as accountService from '../../services/account.service';
  
 export const login = (oidc) => async (ctx) => {
-    const {
-      prompt: { name },
-    } = await oidc.interactionDetails(ctx.req, ctx.res);
+    const { prompt: { name } } = await oidc.interactionDetails(ctx.req, ctx.res);
+
     if (name === 'login') {
       const account = await accountService.get(ctx.request.body.username);
       let result: any;
-      if(!account.email_verified) {
+
+      // Account doesn't exist OR Password doesn't match
+      if (account?.password !== ctx.request.body.password) {
+        result = {
+          error: 'access_denied',
+          error_description: 'Username or password is incorrect.',
+        };
+        ctx.body = result;
+      } 
+
+      // Account haven't verify yet
+      else if (!account.email_verified) {
         result = {
           error: 'unverified',
           error_description: 'Please check your email and verify your email address',
         };
+        ctx.body = result;
       }
-      if (account?.password === ctx.request.body.password) {
+
+      // OK
+      else {        
         result = {
           login: {
             accountId: ctx.request.body.username,
           },
         };
         const redirectTo = await oidc.interactionResult(ctx.req, ctx.res, result);
-
         ctx.body = { redirectTo };
-
-      } else {
-        result = {
-          error: 'access_denied',
-          error_description: 'Username or password is incorrect.',
-        };
-        ctx.body = result;
       }
       
     }
