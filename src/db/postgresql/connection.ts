57a829595208e8c27c27668189658a6d8fb89d81
dang luan <dangluan15112001@gmail.com>
2022-07-22 11:43:52 +0700

    update verify email

--- src/db/postgresql/connection.ts
+++ src/db/postgresql/connection.ts
@@ -2,10 +2,11 @@ import { Pool } from 'pg';
 
 let pool = new Pool({
         user: 'postgres',
-        host: 'localhost',
+        host: '127.0.0.1',
         database: 'DEMO SSO',
         password: 'giacat',
         port: '5432'
 });
+console.log("connect here")
 
 export const db = pool;
